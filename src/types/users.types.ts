9b0dc6d1a3a74a28b4a1698e7593d59ce3bc50a5
Afonso Gouveia <gouvei44@gmail.com>
2023-12-29 16:21:45 -0300

    :sparkles: feat: add types

---
+++ src/types/users.types.ts
@@ -0,0 +1,12 @@
+interface Users {
+    firstName: string;
+    lastName: string;
+    birthDate: string | Date;
+    city: string;
+    country: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }
+  
+  export default Users;
