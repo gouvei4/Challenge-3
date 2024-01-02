692036f89a640e14dc88a5497d6f3435cbb8e9e7
Afonso Gouveia <gouvei44@gmail.com>
2024-01-02 17:00:59 -0300

    :sparkles: feat: added enum to events

--- src/types/events.type.ts
+++ src/types/events.type.ts
@@ -1,6 +1,17 @@
+export enum DayOfWeek {
+    Sunday = 'Sunday',
+    Monday = 'Monday',
+    Tuesday = 'Tuesday',
+    Wednesday = 'Wednesday',
+    Thursday = 'Thursday',
+    Friday = 'Friday',
+    Saturday = 'Saturday',
+  }
+  
 export type Events = {
     description: string;
-    dayOfWeek: string;
+    dayOfWeek: DayOfWeek;
+    userId: string;
 }
 
 export default Events;
