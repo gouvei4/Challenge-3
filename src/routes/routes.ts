d56454f2e693cab88a7a155aca1b15503d32d124
Afonso Gouveia <gouvei44@gmail.com>
2024-01-03 16:13:30 -0300

    :sparkles: feat: created delete route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,7 +3,7 @@ import SignUpController from '../controllers/signup.controller';
 import SigInController from '../controllers/signin.controller';
 import EventsController from '../controllers/events.controller';
 import GetEventsController from '../controllers/get.events.controller';
-
+import DeleteEventsController from '../controllers/delete.events.controller';
 const routes = express.Router();
 
 routes.get('/', function (request, response) {
@@ -14,6 +14,8 @@ routes.use('/users/sign-up', SignUpController.router);
 routes.use('/users/sign-in', SigInController.router);
 routes.use('/users/events', EventsController.router);
 routes.use('/events', GetEventsController.router);
+routes.use('/events', DeleteEventsController.router);
+
 
 
 export default routes;
