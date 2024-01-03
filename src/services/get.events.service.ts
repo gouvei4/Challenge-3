107982361289aea9f12903f3f3a2b03e9624c81e
Afonso Gouveia <gouvei44@gmail.com>
2024-01-03 16:09:11 -0300

    :sparkles: feat: added jwt in get services

--- src/services/get.events.service.ts
+++ src/services/get.events.service.ts
@@ -4,19 +4,30 @@ import Event from '../model/events.model';
 class GetEvents {
   public async getEvents(request: Request, response: Response) {
     try {
-      const  searchDay  = request.query.dayOfWeek;
+      const searchDay = request.query.dayOfWeek;
+      const authorizationHeader = request.headers.authorization;
+      
+      if (!authorizationHeader) {
+        response.status(401).json({
+          success: false,
+          message: 'Access token is missing',
+        });
+        return;
+      }
 
       if (!searchDay) {
         return response.status(400).json({ error: 'Day paramer is missing' });
       }
 
-      const events = await Event.find({ dayOfWeek: searchDay});
+      const events = await Event.find({ dayOfWeek: searchDay });
       return response.status(200).json({ events: events });
-} catch (error: any) {
-    response.status(500).json({
-      success: false,
-      message: error.message.toString(),
-    });
-}}}
+    } catch (error: any) {
+      response.status(500).json({
+        success: false,
+        message: error.message.toString(),
+      });
+    }
+  }
+}
 
 export default new GetEvents();
