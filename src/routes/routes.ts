9a4bf235a1e31703ee3f47b75340e08f6d6788e0
Afonso Gouveia <gouvei44@gmail.com>
2024-01-03 17:35:22 -0300

    :sparkles: feat: added GetEventId router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,18 +4,19 @@ import SigInController from '../controllers/signin.controller';
 import EventsController from '../controllers/events.controller';
 import GetEventsController from '../controllers/get.events.controller';
 import DeleteEventsController from '../controllers/delete.events.controller';
+import GetEventIdController from '../controllers/get.eventsid.controller';
+
 const routes = express.Router();
 
 routes.get('/', function (request, response) {
-    response.json({ API: 'Welcome to JWT Authentication' });
-  });
+  response.json({ API: 'Welcome to JWT Authentication' });
+});
 
 routes.use('/users/sign-up', SignUpController.router);
 routes.use('/users/sign-in', SigInController.router);
 routes.use('/users/events', EventsController.router);
 routes.use('/events', GetEventsController.router);
 routes.use('/events', DeleteEventsController.router);
-
-
+routes.use('/events', GetEventIdController.router);
 
 export default routes;
